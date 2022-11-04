96ca79f9a5de0a04753129626d5f046a69e5d20d
Abdur Rahman <91240493+ruhin645@users.noreply.github.com>
2022-11-04 11:15:08 -0700

    Update Vaccine Dates_CChef.cpp

--- Vaccine Dates_CChef.cpp
+++ Vaccine Dates_CChef.cpp
@@ -1,4 +1,4 @@
-#include bits/stdc++.h
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -7,9 +7,19 @@ int main()
     while(t--)
     {
         cin>>d>>l>>r;
-        if(d>=l && d=<r)
+        if(d>=l && d<=r)
         {
+            
             cout<<"Take second dose now"<<endl;
+           
+        }
+        else if(d>l && d>r)
+        {
+            cout<<"Too Late"<<endl;
+        }
+        else if(d<l && d<r)
+        {
+            cout<<"Too Early"<<endl;
         }
     }
 }
